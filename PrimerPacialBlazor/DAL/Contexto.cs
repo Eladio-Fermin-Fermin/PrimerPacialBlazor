91caa90bcc243ad87a36b70ec77fd4987396d935
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-02-11 04:57:43 -0400

    Se creo la clase Contexto en la carpeta DAL

---
+++ PrimerPacialBlazor/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerPacialBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerPacialBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
