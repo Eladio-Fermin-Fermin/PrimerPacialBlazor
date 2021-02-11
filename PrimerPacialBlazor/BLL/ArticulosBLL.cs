5eafebd7d455ac53bf29eba2973ee797a9e5d807
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-02-11 05:04:16 -0400

    Se creo la clase ArticulosBLL en la carpeta BLL y se añadio el metodo Existe.

---
+++ PrimerPacialBlazor/BLL/ArticulosBLL.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerPacialBlazor.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerPacialBlazor.BLL
+{
+    public class ArticulosBLL
+    {
+        private Contexto contexto { get; set; }
+
+        public ArticulosBLL(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        //Metodo Existe.
+        public async Task<bool> Existe(int id)
+        {
+            bool encontrado = false;
+            try
+            {
+                encontrado = await contexto.Articulos.AnyAsync(a => a.ArticuloId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return encontrado;
+        }
+
+
+
+
+
+    }
+}
