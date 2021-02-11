578d2e8eb8b71637ca5493f445866e79a022b316
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-02-11 04:52:51 -0400

    Se creo la clase articulos en la carpeta Models.

---
+++ PrimerPacialBlazor/Models/Articulos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerPacialBlazor.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introduccir un descripcion.")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introduccir una existencia.")]
+        public int Existencia { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introduccir una costo.")]
+        public double Costo { get; set; }
+        public double ValorInventario { get; set; }
+    }
+}
